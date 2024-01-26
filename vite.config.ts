3a081e388829cfe084f61a4e8c8fc58ea1b8c958
Sebastián Castro <sebastian.castroa25@gmail.com>
2024-01-26 16:34:09 -0500

    base URL correction

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://bluefirea25.github.io/",
+  base: "https://bluefirea25.github.io/3-in-arrow-react",
 });
